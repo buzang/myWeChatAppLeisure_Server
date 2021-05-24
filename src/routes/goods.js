2aa2c8ceafeac9f4294742eed236b60a27ee96a5
nipengcheng <nipc672@foxmail.com>
2021-05-24 16:26:26 +0800

    搭建服务器文件夹基本结构[1]

--- src/routes/goods.js
+++ src/routes/goods.js
@@ -2,6 +2,7 @@
 const querystring = require('querystring')
 const handleGoodsRoute = (req, res) => {
     if (req.method === 'GET' && req.path == '/api/goods/list') {
+        const pagenum = req.query.pagenum
         return {
             message: 'goods_data'
         }
